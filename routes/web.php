547c56fe19c7be05658b1d9699788a26aaf925fc
Faiaz <njfaiaz@gmail.com>
2024-11-11 22:07:24 +0600

    Vue Install

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/admin/dashboard', function () {
+    return view('dashboard');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
